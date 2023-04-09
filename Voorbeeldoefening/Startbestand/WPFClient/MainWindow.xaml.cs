eed6d3ca9c82f4fc5fe15f994c865991daa3ed0a
ChristopheM98 <r0901658@student.thomasmore.be>
2023-04-09 14:57:36 +0200

    Voorbeeldoefening aangepast + verslag uitgebreid

--- Voorbeeldoefening/Startbestand/WPFClient/MainWindow.xaml.cs
+++ Voorbeeldoefening/Startbestand/WPFClient/MainWindow.xaml.cs
@@ -16,7 +16,44 @@ namespace WPFClient
         {
             InitializeComponent();
 
-            
+
+
+            connection.Reconnecting += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = "Attempting to reconnect...";
+                    messages.Items.Add(newMessage);
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = "Reconnected to the server!";
+                    messages.Items.Clear();
+                    messages.Items.Add(newMessage);
+                });
+
+                //Wachten op Dispatcher, dan pas Task completen
+                return Task.CompletedTask;
+            };
+
+            connection.Closed += (sender) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = "Connection closed!";
+                    messages.Items.Add(newMessage);
+                    openConnection.IsEnabled = true;
+                    sendMessage.IsEnabled = false;
+                });
+
+                return Task.CompletedTask;
+            };
         }
 
         private async void openConnection_Click(object sender, RoutedEventArgs e)
